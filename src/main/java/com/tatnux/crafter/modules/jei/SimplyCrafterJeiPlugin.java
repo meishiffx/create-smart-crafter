cceeefa7aedb6446be8f15d3ead89a8579d98bb7
Tatnux <matthieucollet25@gmail.com>
2024-09-14 02:08:47 +0200

    Add Crafting Recipe Catalyst

--- src/main/java/com/tatnux/crafter/modules/jei/SimplyCrafterJeiPlugin.java
+++ src/main/java/com/tatnux/crafter/modules/jei/SimplyCrafterJeiPlugin.java
@@ -1,10 +1,14 @@
 package com.tatnux.crafter.modules.jei;
 
 import com.tatnux.crafter.SimplyCrafter;
+import com.tatnux.crafter.modules.crafter.CrafterModule;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeTransferRegistration;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 @JeiPlugin
@@ -18,5 +22,10 @@ public class SimplyCrafterJeiPlugin implements IModPlugin {
     @Override
     public void registerRecipeTransferHandlers(@NotNull IRecipeTransferRegistration registration) {
         CrafterRecipeTransferHandler.register(registration);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(CrafterModule.CRAFTER), RecipeTypes.CRAFTING);
     }
 }
